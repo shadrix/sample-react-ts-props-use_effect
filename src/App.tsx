80f80519c0caef51e47b0b03b246cca273d799fc
shadrix <postilga.dima@gmail.com>
2021-12-05 23:36:35 +0200

    added sample

---
+++ src/App.tsx
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from 'react';
+
+import Container from 'react-bootstrap/Container';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Badge from 'react-bootstrap/Badge';
+import Spinner from 'react-bootstrap/Spinner';
+
+interface IListItemProps {
+  title: string;
+  description: string;
+  count: number;
+}
+
+const ListItem = (props: IListItemProps) => (
+          <ListGroup.Item as="li"
+                          className="d-flex justify-content-between align-items-start">
+                            <div className="ms-2 me-auto">
+                              <div className="fw-bold">{props.title}</div>
+                              {props.description}
+                            </div>
+                          <Badge bg="primary" pill>
+                              {props.count}
+                          </Badge>
+          </ListGroup.Item>
+  );
+
+interface IListProps {
+    children: React.ReactNode;
+}
+
+const List = (props: IListProps) => (
+  <ListGroup as="ol" numbered>
+    {props.children}
+  </ListGroup>
+)
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [header, setHeader] = useState('');
+
+  function sleep(ms : number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  async function longOperation() : Promise<string> {
+    const text = "Custom list";
+    await sleep(2000);
+    return text;
+  }
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+
+    async function init() {
+      const result = await longOperation();
+      setHeader(result);
+      setLoading(false);
+    }
+
+    init();
+  });
+
+  return (
+    <Container className="p-3">
+      <Container className="pb-1 p-5 mb-4 bg-light rounded-3">
+        {loading && <span>Loading...</span>
+        }
+        {loading 
+                ? <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                  </Spinner>
+                :
+                <>
+                  <h1 className="header">{header}</h1>
+                  <List>
+                    <ListItem title="bla bla" description="moo noo loo" count={4}  />
+                    <ListItem title="bla bla" description="moo noo loo" count={4}  />
+                    <ListItem title="bla bla" description="moo noo loo" count={4}  />
+                    <ListItem title="bla bla" description="moo noo loo" count={4}  />
+                    <ListItem title="bla bla" description="moo noo loo" count={4}  />
+                    <ListItem title="bla bla" description="moo noo loo" count={4}  />
+                    <ListItem title="bla bla" description="moo noo loo" count={4}  />
+                  </List>
+                </>
+        }
+        
+      </Container>
+    </Container>
+  );
+}
+
+
+export default App;
